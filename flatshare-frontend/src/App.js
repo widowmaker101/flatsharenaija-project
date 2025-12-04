f140e1827dadf18d3051627cc07473461f1bfa3e
Charles Ogbonnia <73829197+widowmaker101@users.noreply.github.com>
2025-12-04 18:49:08 +0100

    Integrate CreateListing and Listings components

---
+++ flatshare-frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import CreateListing from "./CreateListing";
+import Listings from "./Listings";
+
+function App() {
+  return (
+    <div>
+      <h1>FlatshareNaija</h1>
+      <CreateListing />
+      <Listings />
+    </div>
+  );
+}
+
+export default App;

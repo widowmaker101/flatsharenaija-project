5fd9793b5ef5826a75cf0c4336c2f441173a3f96
Charles Ogbonnia <73829197+widowmaker101@users.noreply.github.com>
2025-12-04 17:24:48 +0100

    Force use of DATABASE_URL, remove SQLite fallback

--- api/database.py
+++ api/database.py
@@ -1,16 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./db/flatshare.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set!")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()

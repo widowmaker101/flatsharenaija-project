b845803a5b0660ac92ec7534bed53895aec443dc
Charles Ogbonnia <73829197+widowmaker101@users.noreply.github.com>
2025-12-04 18:28:23 +0100

    Add Listing model

--- api/models.py
+++ api/models.py
@@ -8,3 +8,20 @@ class User(Base):
     username = Column(String, unique=True, index=True, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
+
+class Listing(Base):
+    __tablename__ = "listings"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String)
+    location = Column(String, index=True)
+    price = Column(Float)
+    availability = Column(String)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("User", back_populates="listings")
+
+User.listings = relationship("Listing", back_populates="owner")

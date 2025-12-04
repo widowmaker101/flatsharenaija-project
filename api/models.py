367c273ef0f62d4e630be6b6f3dfdd7daa7cc033
Charles Ogbonnia <73829197+widowmaker101@users.noreply.github.com>
2025-12-04 16:15:25 +0100

    Add User model

--- api/models.py
+++ api/models.py
@@ -1,21 +1,10 @@
-from sqlalchemy import Column, Integer, String, Text, TIMESTAMP
-from .database import Base
-
-class Listing(Base):
-    __tablename__ = "listings"
-
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    description = Column(Text)
-    city = Column(String, index=True)
-    area = Column(String, index=True)
-    rent = Column(Integer)
-    created_at = Column(TIMESTAMP)
-    updated_at = Column(TIMESTAMP)
+from sqlalchemy import Column, Integer, String
+from database import Base
 
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    password = Column(String)
+    username = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
